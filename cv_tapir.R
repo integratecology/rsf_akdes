bffadcf3780d19dc0cb7458df40d1c12d2a1e476
integratecology <j.alston@hzdr.de>
2022-04-29 10:00:45 +0200

    Editing tapir scripts

--- cv_tapir.R
+++ cv_tapir.R
@@ -44,8 +44,16 @@ print("Fitted movement model")
 # Calculate the habitat-naive AKDE UD ###
 ud <- akde(train, fit, weights=TRUE)
 ess <- summary(ud)$DOF["area"]
-ud95_area <- summary(ud, level.UD=0.95, units=FALSE)$CI[1,2]
+ud10_area <- summary(ud, level.UD=0.10, units=FALSE)$CI[1,2]
+ud20_area <- summary(ud, level.UD=0.20, units=FALSE)$CI[1,2]
+ud30_area <- summary(ud, level.UD=0.30, units=FALSE)$CI[1,2]
+ud40_area <- summary(ud, level.UD=0.40, units=FALSE)$CI[1,2]
 ud50_area <- summary(ud, level.UD=0.50, units=FALSE)$CI[1,2]
+ud60_area <- summary(ud, level.UD=0.60, units=FALSE)$CI[1,2]
+ud70_area <- summary(ud, level.UD=0.70, units=FALSE)$CI[1,2]
+ud80_area <- summary(ud, level.UD=0.80, units=FALSE)$CI[1,2]
+ud90_area <- summary(ud, level.UD=0.90, units=FALSE)$CI[1,2]
+ud99_area <- summary(ud, level.UD=0.99, units=FALSE)$CI[1,2]
 print("UD created")
 
 # Fit the RSF ###
@@ -55,38 +63,82 @@ print("Fitted RSF")
 
 # Calculate the RSF-informed AKDE
 ud_rsf <- akde(train, rsf, R=list(trees=r2))
-ud95_rsf_area <- summary(ud_rsf, level.UD=0.95, units=FALSE)$CI[1,2]
+ud10_rsf_area <- summary(ud_rsf, level.UD=0.10, units=FALSE)$CI[1,2]
+ud20_rsf_area <- summary(ud_rsf, level.UD=0.20, units=FALSE)$CI[1,2]
+ud30_rsf_area <- summary(ud_rsf, level.UD=0.30, units=FALSE)$CI[1,2]
+ud40_rsf_area <- summary(ud_rsf, level.UD=0.40, units=FALSE)$CI[1,2]
 ud50_rsf_area <- summary(ud_rsf, level.UD=0.50, units=FALSE)$CI[1,2]
+ud60_rsf_area <- summary(ud_rsf, level.UD=0.60, units=FALSE)$CI[1,2]
+ud70_rsf_area <- summary(ud_rsf, level.UD=0.70, units=FALSE)$CI[1,2]
+ud80_rsf_area <- summary(ud_rsf, level.UD=0.80, units=FALSE)$CI[1,2]
+ud90_rsf_area <- summary(ud_rsf, level.UD=0.90, units=FALSE)$CI[1,2]
+ud99_rsf_area <- summary(ud_rsf, level.UD=0.99, units=FALSE)$CI[1,2]
 
 # Cross-validate the UDs ###
 
 # Export the UDs as sp objects
-ud95 <- SpatialPolygonsDataFrame.UD(ud, level.UD = 0.95)
+ud10 <- SpatialPolygonsDataFrame.UD(ud, level.UD = 0.10)
+ud20 <- SpatialPolygonsDataFrame.UD(ud, level.UD = 0.20)
+ud30 <- SpatialPolygonsDataFrame.UD(ud, level.UD = 0.30)
+ud40 <- SpatialPolygonsDataFrame.UD(ud, level.UD = 0.40)
 ud50 <- SpatialPolygonsDataFrame.UD(ud, level.UD = 0.50)
+ud60 <- SpatialPolygonsDataFrame.UD(ud, level.UD = 0.60)
+ud70 <- SpatialPolygonsDataFrame.UD(ud, level.UD = 0.70)
+ud80 <- SpatialPolygonsDataFrame.UD(ud, level.UD = 0.80)
+ud90 <- SpatialPolygonsDataFrame.UD(ud, level.UD = 0.90)
+ud99 <- SpatialPolygonsDataFrame.UD(ud, level.UD = 0.99)
 
-ud95_rsf <- SpatialPolygonsDataFrame.UD(ud_rsf, level.UD = 0.95)
+ud10_rsf <- SpatialPolygonsDataFrame.UD(ud_rsf, level.UD = 0.10)
+ud20_rsf <- SpatialPolygonsDataFrame.UD(ud_rsf, level.UD = 0.20)
+ud30_rsf <- SpatialPolygonsDataFrame.UD(ud_rsf, level.UD = 0.30)
+ud40_rsf <- SpatialPolygonsDataFrame.UD(ud_rsf, level.UD = 0.40)
 ud50_rsf <- SpatialPolygonsDataFrame.UD(ud_rsf, level.UD = 0.50)
+ud60_rsf <- SpatialPolygonsDataFrame.UD(ud_rsf, level.UD = 0.60)
+ud70_rsf <- SpatialPolygonsDataFrame.UD(ud_rsf, level.UD = 0.70)
+ud80_rsf <- SpatialPolygonsDataFrame.UD(ud_rsf, level.UD = 0.80)
+ud90_rsf <- SpatialPolygonsDataFrame.UD(ud_rsf, level.UD = 0.90)
+ud99_rsf <- SpatialPolygonsDataFrame.UD(ud_rsf, level.UD = 0.99)
 
 # Export the test data as an sp object
 test_sp <- SpatialPoints.telemetry(test)
 
 # Determine which points are inside the 95% OD
-pct95 <- round(unname(GISTools:::poly.counts(test_sp, ud95)[2])/nrow(test)*100, 2)
+pct10 <- round(unname(GISTools:::poly.counts(test_sp, ud10)[2])/nrow(test)*100, 2)
+pct20 <- round(unname(GISTools:::poly.counts(test_sp, ud20)[2])/nrow(test)*100, 2)
+pct30 <- round(unname(GISTools:::poly.counts(test_sp, ud30)[2])/nrow(test)*100, 2)
+pct40 <- round(unname(GISTools:::poly.counts(test_sp, ud40)[2])/nrow(test)*100, 2)
 pct50 <- round(unname(GISTools:::poly.counts(test_sp, ud50)[2])/nrow(test)*100, 2)
+pct60 <- round(unname(GISTools:::poly.counts(test_sp, ud60)[2])/nrow(test)*100, 2)
+pct70 <- round(unname(GISTools:::poly.counts(test_sp, ud70)[2])/nrow(test)*100, 2)
+pct80 <- round(unname(GISTools:::poly.counts(test_sp, ud80)[2])/nrow(test)*100, 2)
+pct90 <- round(unname(GISTools:::poly.counts(test_sp, ud90)[2])/nrow(test)*100, 2)
+pct99 <- round(unname(GISTools:::poly.counts(test_sp, ud99)[2])/nrow(test)*100, 2)
 
-pct95_rsf <- round(unname(GISTools:::poly.counts(test_sp, ud95_rsf)[2])/nrow(test)*100, 2)
+pct10_rsf <- round(unname(GISTools:::poly.counts(test_sp, ud10_rsf)[2])/nrow(test)*100, 2)
+pct20_rsf <- round(unname(GISTools:::poly.counts(test_sp, ud20_rsf)[2])/nrow(test)*100, 2)
+pct30_rsf <- round(unname(GISTools:::poly.counts(test_sp, ud30_rsf)[2])/nrow(test)*100, 2)
+pct40_rsf <- round(unname(GISTools:::poly.counts(test_sp, ud40_rsf)[2])/nrow(test)*100, 2)
 pct50_rsf <- round(unname(GISTools:::poly.counts(test_sp, ud50_rsf)[2])/nrow(test)*100, 2)
+pct60_rsf <- round(unname(GISTools:::poly.counts(test_sp, ud60_rsf)[2])/nrow(test)*100, 2)
+pct70_rsf <- round(unname(GISTools:::poly.counts(test_sp, ud70_rsf)[2])/nrow(test)*100, 2)
+pct80_rsf <- round(unname(GISTools:::poly.counts(test_sp, ud80_rsf)[2])/nrow(test)*100, 2)
+pct90_rsf <- round(unname(GISTools:::poly.counts(test_sp, ud90_rsf)[2])/nrow(test)*100, 2)
+pct99_rsf <- round(unname(GISTools:::poly.counts(test_sp, ud99_rsf)[2])/nrow(test)*100, 2)
 
 eTime <- Sys.time()
   
 # Create vector of results
-results <- data.frame(aid, ind_file, ess, ud95_area, ud50_area, pct95, pct50, ud95_rsf_area, ud50_rsf_area, pct95_rsf, pct50_rsf)
+results <- data.frame(aid, ind_file, ess, 
+		      ud10_area, ud20_area, ud30_area, ud40_area, ud50_area, ud60_area, ud70_area, ud80_area, ud90_area, ud99_area, 
+		      ud10_rsf_area, ud20_rsf_area, ud30_rsf_area, ud40_rsf_area, ud50_rsf_area, ud60_rsf_area, ud70_rsf_area, ud80_rsf_area, ud90_rsf_area, ud99_rsf_area,
+		      pct10, pct20, pct30, pct40, pct50, pct60, pct70, pct80, pct90, pct99,
+		      pct10_rsf, pct20_rsf, pct30_rsf, pct40_rsf, pct50_rsf, pct60_rsf, pct70_rsf, pct80_rsf, pct90_rsf, pct99_rsf)
   
 # Store results in data.frame
 write.table(results, 'results/cv_summary_tapir.csv', append=TRUE, row.names=FALSE, col.names=FALSE, sep=',') 
 
 # Save important objects to an Rda file
-save(df,svf,fit,ud,rsf,ud_rsf,file=paste0(aid,".Rda"))
+save(df,svf,fit,ud,rsf,ud_rsf,file=paste0("results/",aid,".Rda"))
   
 # Print indicators of progress
 print(aid)
--- run_cv_tapir.sh
+++ run_cv_tapir.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=tapir_cv        # name of the job
 #SBATCH --partition=defq,intel     # partition to be used (defq, gpu or intel)
-#SBATCH --time=96:00:00            # walltime (up to 96 hours)
+#SBATCH --time=4:00:00             # walltime (up to 96 hours)
 #SBATCH --nodes=1                  # number of nodes
 #SBATCH --ntasks-per-node=1        # number of tasks (i.e. parallel processes) to be started
 #SBATCH --cpus-per-task=1          # number of cpus required to run the script
@@ -32,7 +32,7 @@ if [ -f results/cv_summary_tapir.csv ]; then
 	echo "Results file already exists! continuing..."
 else
 	echo "creating results file cv_summary.csv"
-	echo "aid,ind_file,ess,ud95_area,ud50_area,pct95,pct50,ud95_rsf_area,ud_50_rsf_area,pct95_rsf,pct50_rsf" > results/cv_summary_tapir.csv
+	echo "id, ind_file, ess, ud10_area, ud20_area, ud30_area, ud40_area, ud50_area, ud60_area, ud70_area, ud80_area, ud90_area, ud99_area, ud10_rsf_area, ud20_rsf_area, ud30_rsf_area, ud40_rsf_area, ud50_rsf_area, ud60_rsf_area, ud70_rsf_area, ud80_rsf_area, ud90_rsf_area, ud99_rsf_area, pct10, pct20, pct30, pct40, pct50, pct60, pct70, pct80, pct90, pct99, pct10_rsf, pct20_rsf, pct30_rsf, pct40_rsf, pct50_rsf, pct60_rsf, pct70_rsf, pct80_rsf, pct90_rsf, pct99_rsf" > results/cv_summary_tapir.csv
 fi
 
 
